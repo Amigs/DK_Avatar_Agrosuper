c8fda3e83734617c533e324dc022cb01da36bcab
Alexander Olier <alexander.olier@dataknow.co>
2025-09-26 16:26:29 -0500

    agregar docker al frontend

---
+++ frontend/dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Nombre de la imagen
+IMAGE_NAME="frontend_agrosuper_avatar:latest"
+
+echo "🔨 Construyendo la imagen Docker..."
+docker build -t $IMAGE_NAME .
+
+if [ $? -ne 0 ]; then
+  echo "❌ Error al construir la imagen. Abortando."
+  exit 1
+fi
+
+echo "🚀 Ejecutando el contenedor..."
+docker run --rm -it -p 8001:80 $IMAGE_NAME
+
+echo "✅ El contenedor está corriendo en: http://localhost:$PORT"
